a1bc9af8451b5a3996774f711354afdcb19eb4e1
YongminK <yongminni97@gmail.com>
2019-09-19 23:52:01 +0300

    addes Specialists section
    wirhout adaptive

---
+++ src/components/home/Specialists.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Specialists extends React.Component{
+    render(){
+        return(
+            <section className="specialists">
+                <div className="specialists__head">
+                    <p>Наши специалисты</p>
+                </div>
+                <div className="specialists__logo">
+                    <div className="specialists__logo__img"></div>
+                    <p className="specialists__logo__desc">Психотеропевт</p>
+                </div>
+                <div className="specialists__name">
+                    <p>Елизавета Сергеевна</p>
+                </div>
+                <div className="specialists__description">
+                    <p className="specialists__description__experience">Опыт работы: 25 лет</p>
+                    <p className="specialists__description__study">
+                        Окончила Ярославский медицинский университет.
+                    </p>
+                    <p className="specialists__description__text">
+                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                    </p>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Specialists;
