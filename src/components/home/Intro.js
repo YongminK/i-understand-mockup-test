703abe5c1f9a631c4e4492a868d7626259246dc3
YongminK <yongminni97@gmail.com>
2019-09-19 23:51:07 +0300

    added Intro section
    without adaptive

---
+++ src/components/home/Intro.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Intro extends React.Component {
+    render() {
+        return (
+            <section className="intro">
+                <div className="intro__introduce">
+                    <p className="introduce__text">
+                        Мы служба дистанционной психологической помощи.
+                    </p>
+                </div>
+                <div className="intro__description">
+                    <p className="description__text">
+                        Мы команда психологов, психотерапевтов, менеджеров и других
+                        специалистов – созданы для того, чтобы помочь любому человеку
+                        стать хотя бы немного счастливее.
+                    </p>
+                </div>
+                <div className="intro__button">
+                    <a href="#" className="button__green">Подробнее</a>
+                </div>
+            </section>
+        )
+    }
+}
+
+export default Intro;
