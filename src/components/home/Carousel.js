b5dbdf1d66eb25fa71eb9fa62d8c4ecdef63e795
Hellen <yongminni97@gmail.com>
2019-09-22 10:21:30 +0300

    Carousel

---
+++ src/components/home/Carousel.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import Slider from "react-slick";
+import '../../style/slick.min.css';
+import '../../style/slick-theme.min.css';
+
+export default class Carousel extends React.Component {
+    render() {
+        var settings = {
+            dots: true
+          };
+        return (
+            <Slider {...settings}>
+                <div className="carousel__item">
+                    <div className="specialists__logo">
+                        <div className="specialists__logo__img"></div>
+                        <p className="specialists__logo__desc">Психотеропевт</p>
+                    </div>
+                    <div className="specialists__name">
+                        <p>Елизавета Сергеевна</p>
+                    </div>
+                    <div className="specialists__description">
+                        <p className="specialists__description__experience">
+                            Опыт работы: 25 лет
+                                        </p>
+                        <p className="specialists__description__study">
+                            Окончила Ярославский медицинский университет.
+                                        </p>
+                        <p className="specialists__description__text">
+                            Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                                        </p>
+                    </div>
+                </div>
+                <div className="carousel__item">
+                    <div className="specialists__logo">
+                        <div className="specialists__logo__img"></div>
+                        <p className="specialists__logo__desc">Психотеропевт</p>
+                    </div>
+                    <div className="specialists__name">
+                        <p>Елизавета Сергеевна</p>
+                    </div>
+                    <div className="specialists__description">
+                        <p className="specialists__description__experience">Опыт работы: 25 лет</p>
+                        <p className="specialists__description__study">
+                            Окончила Ярославский медицинский университет.
+                                        </p>
+                        <p className="specialists__description__text">
+                            Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                                        </p>
+                    </div>
+                </div>
+                <div className="carousel__item">
+                    <div className="specialists__logo">
+                        <div className="specialists__logo__img"></div>
+                        <p className="specialists__logo__desc">Психотеропевт</p>
+                    </div>
+                    <div className="specialists__name">
+                        <p>Елизавета Сергеевна</p>
+                    </div>
+                    <div className="specialists__description">
+                        <p className="specialists__description__experience">Опыт работы: 25 лет</p>
+                        <p className="specialists__description__study">
+                            Окончила Ярославский медицинский университет.
+                                        </p>
+                        <p className="specialists__description__text">
+                            Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                                        </p>
+                    </div>
+                </div>
+                <div className="carousel__item">
+                    <div className="specialists__logo">
+                        <div className="specialists__logo__img"></div>
+                        <p className="specialists__logo__desc">Психотеропевт</p>
+                    </div>
+                    <div className="specialists__name">
+                        <p>Елизавета Сергеевна</p>
+                    </div>
+                    <div className="specialists__description">
+                        <p className="specialists__description__experience">Опыт работы: 25 лет</p>
+                        <p className="specialists__description__study">
+                            Окончила Ярославский медицинский университет.
+                                        </p>
+                        <p className="specialists__description__text">
+                            Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                                        </p>
+                    </div>
+                </div>
+                <div className="carousel__item">
+                    <div className="specialists__logo">
+                        <div className="specialists__logo__img"></div>
+                        <p className="specialists__logo__desc">Психотеропевт</p>
+                    </div>
+                    <div className="specialists__name">
+                        <p>Елизавета Сергеевна</p>
+                    </div>
+                    <div className="specialists__description">
+                        <p className="specialists__description__experience">Опыт работы: 25 лет</p>
+                        <p className="specialists__description__study">
+                            Окончила Ярославский медицинский университет.
+                                        </p>
+                        <p className="specialists__description__text">
+                            Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
+                    </p>
+                    </div>
+                </div>
+            </Slider>
+        );
+    }
+}
+
--- src/components/home/Specialists.js
+++ src/components/home/Specialists.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Carousel from './Carousel';
 
 class Specialists extends React.Component {
     render() {
@@ -7,105 +8,7 @@ class Specialists extends React.Component {
                 <div className="specialists__head">
                     <p>Наши специалисты</p>
                 </div>               
-                <div className="specialists__carousel">
-                    <div className="carousel__items">
-                        <ul>
-                            <li className="carousel__item">                                
-                                <div className="specialists__logo">
-                                    <div className="specialists__logo__img"></div>
-                                    <p className="specialists__logo__desc">Психотеропевт</p>
-                                </div>
-                                <div className="specialists__name">
-                                    <p>Елизавета Сергеевна</p>
-                                </div>
-                                <div className="specialists__description">
-                                    <p className="specialists__description__experience">
-                                        Опыт работы: 25 лет
-                                    </p>
-                                    <p className="specialists__description__study">
-                                        Окончила Ярославский медицинский университет.
-                                    </p>
-                                    <p className="specialists__description__text">
-                                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
-                                    </p>
-                                </div>
-                            </li>
-                            <li className="carousel__item">
-                                <div className="specialists__logo">
-                                    <div className="specialists__logo__img"></div>
-                                    <p className="specialists__logo__desc">Психотеропевт</p>
-                                </div>
-                                <div className="specialists__name">
-                                    <p>Елизавета Сергеевна</p>
-                                </div>
-                                <div className="specialists__description">
-                                    <p className="specialists__description__experience">Опыт работы: 25 лет</p>
-                                    <p className="specialists__description__study">
-                                        Окончила Ярославский медицинский университет.
-                                    </p>
-                                    <p className="specialists__description__text">
-                                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
-                                    </p>
-                                </div>
-                            </li>
-                            <li className="carousel__item">                                
-                                <div className="specialists__logo">
-                                    <div className="specialists__logo__img"></div>
-                                    <p className="specialists__logo__desc">Психотеропевт</p>
-                                </div>
-                                <div className="specialists__name">
-                                    <p>Елизавета Сергеевна</p>
-                                </div>
-                                <div className="specialists__description">
-                                    <p className="specialists__description__experience">Опыт работы: 25 лет</p>
-                                    <p className="specialists__description__study">
-                                        Окончила Ярославский медицинский университет.
-                                    </p>
-                                    <p className="specialists__description__text">
-                                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
-                                    </p>
-                                </div>
-                            </li>
-                            <li className="carousel__item">                                
-                                <div className="specialists__logo">
-                                    <div className="specialists__logo__img"></div>
-                                    <p className="specialists__logo__desc">Психотеропевт</p>
-                                </div>
-                                <div className="specialists__name">
-                                    <p>Елизавета Сергеевна</p>
-                                </div>
-                                <div className="specialists__description">
-                                    <p className="specialists__description__experience">Опыт работы: 25 лет</p>
-                                    <p className="specialists__description__study">
-                                        Окончила Ярославский медицинский университет.
-                                    </p>
-                                    <p className="specialists__description__text">
-                                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
-                                    </p>
-                                </div>
-                            </li>
-                            <li className="carousel__item">                                
-                                <div className="specialists__logo">
-                                    <div className="specialists__logo__img"></div>
-                                    <p className="specialists__logo__desc">Психотеропевт</p>
-                                </div>
-                                <div className="specialists__name">
-                                    <p>Елизавета Сергеевна</p>
-                                </div>
-                                <div className="specialists__description">
-                                    <p className="specialists__description__experience">Опыт работы: 25 лет</p>
-                                    <p className="specialists__description__study">
-                                        Окончила Ярославский медицинский университет.
-                                    </p>
-                                    <p className="specialists__description__text">
-                                        Работала в поликлинике №2. Врачом стать мечтала, эта профессия нравится потому, что она нужна людям. После работы много гуляю пешком и занимаюсь спортом – врач должен быть в форме.
-                                    </p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="nav__dots"></div>
-                </div>
+                <Carousel/>
 
             </section>
         )
